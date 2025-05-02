5c5dd1ab549002c39a419c838325e784c84ab67e
Hautbois <Promathis.hautbois@gmail.com>
2025-05-02 21:41:30 +0400

    Ajout d'une documentation API avec Swagger dans index.php et gestion des requêtes HTTP.

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,9 +1,41 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $request = Request::createFromGlobals();
+    
+    if ($request->getPathInfo() === '/') {
+        $html = <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>API Documentation</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css">
+        </head>
+        <body>
+            <div id="swagger-ui"></div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js"></script>
+            <script>
+                window.onload = () => {
+                    const ui = SwaggerUIBundle({
+                        url: "/api/doc.json",
+                        dom_id: '#swagger-ui',
+                    });
+                };
+            </script>
+        </body>
+        </html>
+        HTML;
+
+        return new Response($html);
+    }
+
+    return $kernel->handle($request);
 };
